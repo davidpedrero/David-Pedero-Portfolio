802553358c812cac4071e7749c2dfc2a734602fc
David Pedrero <pedrero888@gmail.com>
2022-11-07 15:08:11 -0500

    Please work

--- src/components/Projects/Projects.js
+++ src/components/Projects/Projects.js
@@ -9,7 +9,7 @@ import { projects } from '../../constants/constants';
 const Projects = () => (
 <Section nopadding id="projects">
   <SectionDivider/>
-  <SectionTitle main>Projects 12345</SectionTitle>
+  <SectionTitle main>Projects</SectionTitle>
   <GridContainer>
     {projects.map(({id, image, title, description, tags, source, visit}) => (
       <BlogCard key={id}>
