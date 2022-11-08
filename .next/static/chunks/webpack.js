d191138959ea5e0815891995ab6f8a8a799fdb41
David Pedrero <pedrero888@gmail.com>
2022-11-07 21:28:13 -0500

    fix5

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -182,7 +182,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "6c6fada3cbac5eb6dfb0"; }
+/******/ 		__webpack_require__.h = function() { return "941e005134e8a02b3487"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
--- .next/static/webpack/webpack.04e3685a2484c7cebdb5.hot-update.js
+++ .next/static/webpack/webpack.04e3685a2484c7cebdb5.hot-update.js
@@ -12,7 +12,7 @@ self["webpackHotUpdate_N_E"]("webpack",{},
 /******/ 
 /******/ /* webpack/runtime/getFullHash */
 /******/ !function() {
-/******/ 	__webpack_require__.h = function() { return "e2769180a0dc8aaec10f"; }
+/******/ 	__webpack_require__.h = function() { return "941e005134e8a02b3487"; }
 /******/ }();
 /******/ 
 /******/ /* webpack/runtime/compat */
