dbfc25bcb14f5ed787a190c7a9a09cfdca95b10a
David Pedrero <pedrero888@gmail.com>
2022-11-08 11:57:52 -0500

    Portfolio FE complete

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -182,7 +182,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "b36307c54b2eafed20a1"; }
+/******/ 		__webpack_require__.h = function() { return "fb79085fe11e95d1efb3"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
--- src/constants/constants.js
+++ src/constants/constants.js
@@ -1,54 +1,53 @@
 export const projects = [
   {
-    title: 'MERN Memories',
-    description: "Using React, Node.js, Express & MongoDB you'll learn how to build a Full Stack MERN Application - from start to finish. The App is called Memories and it is a simple social media app that allows users to post interesting events that happened in their lives.",
+    title: 'Face Recognition Brain',
+    description: "Face Recognition Brain is a web app that detects the human face present in the image whose URL you submitted",
       image: '/images/1.png',
-      tags: ['Mongo', 'Express', 'React', 'Node'],
+      tags: ['PostgresSQL', 'Express', 'React', 'Node.js'],
     source: 'https://google.com',
     visit: 'https://google.com',
     id: 0,
   },
   {
-    title: 'E-Commerce',
-    description:"While building it you're going to learn many advanced React & JavaScript topics, as well as how to use Stripe for card transactions. On top of that, at the end of the video, you will have this unique and complex webshop app that you will be able to add to your portfolio. And trust me, e-commerce applications are impressive.",
+    title: 'RoboFriends',
+    description:"A React.js web application that allows you to search bwtween different API generated robots by name",
     image: '/images/2.png',
     tags: ['React', 'JavaScript'],
     source: 'https://google.com',
     visit: 'https://google.com',
     id: 1,
-  },
-  {
-    title: 'WebRTC App',
-    description: "This is a code repository for the corresponding YouTube video. In this tutorial, we're going to build and deploy a React Video Chat Application using WebRTC.",
-      image: '/images/3.jpg',
-      tags: ['React', 'WebRTC'],
-    source: 'https://google.com',
-    visit: 'https://google.com',
-    id: 2,
-  },
-  {
-    title: 'Unichat',
-    description: "This is a code repository for the corresponding video tutorial. In this video, we will create a full Realtime Chat Application",
-    image: '/images/4.jpg',
-    tags: ['React', 'ChatEngine', 'Firebase'],
-    source: 'https://google.com',
-    visit: 'https://google.com',
-    id: 3,
-  },
+  // },
+  // {
+  //   title: 'WebRTC App',
+  //   description: "This is a code repository for the corresponding YouTube video. In this tutorial, we're going to build and deploy a React Video Chat Application using WebRTC.",
+  //     image: '/images/3.jpg',
+  //     tags: ['React', 'WebRTC'],
+  //   source: 'https://google.com',
+  //   visit: 'https://google.com',
+  //   id: 2,
+  // },
+  // {
+  //   title: 'Unichat',
+  //   description: "This is a code repository for the corresponding video tutorial. In this video, we will create a full Realtime Chat Application",
+  //   image: '/images/4.jpg',
+  //   tags: ['React', 'ChatEngine', 'Firebase'],
+  //   source: 'https://google.com',
+  //   visit: 'https://google.com',
+  //   id: 3,
+  }
 ];
 
 export const TimeLineData = [
-  { year: 2017, text: 'Started my journey', },
-  { year: 2018, text: 'Worked as a freelance developer', },
-  { year: 2019, text: 'Founded JavaScript Mastery', },
-  { year: 2020, text: 'Shared my projects with the world', },
-  { year: 2021, text: 'Started my own platform', },
+  { year: 2019, text: 'Started my coding journey', },
+  { year: 2020, text: 'Developed RoboFriends web app', },
+  { year: 2021, text: 'Started at Lotto.com as QA Analyst', },
+  { year: 2022, text: 'Developed Face Recognition Brain web app', }
 ];
 
 
 export const data = [
-  { number: 20, text: 'Open Source Projects'},
-  { number: 1000, text: 'Students', },
-  { number: 1900, text: 'Github Followers', },
-  { number: 5000, text: 'Github Stars', }
+  { number: 200, text: 'GitHub Commits', },
+  { number: 500, text: 'Authored Confluence Articles' },
+  { number: 10, text: 'Open Source Projects',},
+  { number: 3, text: 'Years of Coding Experience' }
 ];
