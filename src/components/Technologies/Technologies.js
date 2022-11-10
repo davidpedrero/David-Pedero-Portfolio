17ffa0bef4b98978293188898dce4f7c0c937b9a
David Pedrero <pedrero888@gmail.com>
2022-11-10 11:49:41 -0500

    small FE change2

--- src/components/Technologies/Technologies.js
+++ src/components/Technologies/Technologies.js
@@ -18,7 +18,7 @@ const Technologies = () =>  (
           <ListTitle>Front-End</ListTitle>
           <ListParagraph>
             Experience with <br/>
-            React.js
+            React and Javascript
           </ListParagraph>
         </ListContainer>
       </ListItem>
