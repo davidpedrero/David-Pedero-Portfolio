1de5cb38a0c6cf5cfd6cf015d3683d4afddb16ee
David Pedrero <pedrero888@gmail.com>
2022-12-24 15:17:13 -0500

    final touch for real this time 5

--- src/components/Hero/Hero.js
+++ src/components/Hero/Hero.js
@@ -8,12 +8,13 @@ const Hero = (props) => (
   <Section row nopadding>
     <LeftSection>
       <SectionTitle main centerssss>
-        Welcome to <br/>
+        Welcome to
+        <br/><br/>
         My Portfolio
       </SectionTitle>
       <SectionText>
         Hi, my name is David Pedrero.
-        <br/>
+        <br/><br/>
         My mission is to develop high quality web solutions that enable my clients to establish online presence, increase profitability and continually improve productivity.
       </SectionText>
       <Button onClick={() => window.open('https://drive.google.com/file/d/10GhcYb00aFA9-gN5lapS2oFnaxztfbkq/view?usp=sharing', '_blank')}>View Resume</Button>
